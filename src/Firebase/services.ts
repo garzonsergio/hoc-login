190b14f5199d137a5a7c64f14cd0b2fc639d3e83
Sergio C Garzon <scgarzonp@gmail.com>
2023-02-21 23:03:20 -0500

    There was created the services to login and sign with email and google account

--- src/Firebase/services.ts
+++ src/Firebase/services.ts
@@ -1,5 +1,15 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { FirebaseAuth } from "./config";
+
+interface PropsRegister {
+  email: string;
+  password: string;
+}
 
 const googleProvider = new GoogleAuthProvider();
 
@@ -9,6 +19,38 @@ export const signInWithGoogle = async () => {
     const { displayName, email, photoURL, uid } = result.user;
 
     return uid;
+  } catch (error) {
+    alert((error as Error).message);
+  }
+};
+
+export const signInWithCredetials = async ({
+  email,
+  password,
+}: PropsRegister) => {
+  try {
+    const result = await createUserWithEmailAndPassword(
+      FirebaseAuth,
+      email,
+      password
+    );
+    return result.user.uid;
+  } catch (error) {
+    alert((error as Error).message);
+  }
+};
+
+export const loginWithCredetials = async ({
+  email,
+  password,
+}: PropsRegister) => {
+  try {
+    const result = await signInWithEmailAndPassword(
+      FirebaseAuth,
+      email,
+      password
+    );
+    return result.user.uid;
   } catch (error) {
     alert((error as Error).message);
   }
